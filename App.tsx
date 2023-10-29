1e89f3818aeac41b93486e5991b39557df261857
ludwigsw <ludwig.wiren@rocker.coml>
2023-10-29 16:13:41 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import {
+  ConnectWallet,
+  localWallet,
+  metamaskWallet,
+  rainbowWallet,
+  ThirdwebProvider,
+} from "@thirdweb-dev/react-native";
+import React from "react";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { TW_CLIENT_ID } from "@env";
+
+const App = () => {
+  return (
+    <ThirdwebProvider
+      // clientId={TW_CLIENT_ID} // uncomment this line after you set your clientId in the .env file
+      activeChain="mumbai"
+      supportedWallets={[metamaskWallet(), rainbowWallet(), localWallet()]}
+    >
+      <AppInner />
+    </ThirdwebProvider>
+  );
+};
+
+const AppInner = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const textStyles = {
+    color: isDarkMode ? Colors.white : Colors.black,
+    ...styles.heading,
+  };
+
+  return (
+    <View style={styles.view}>
+      <Text style={textStyles}>React Native thirdweb starter</Text>
+      <ConnectWallet />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
+
+export default App;
