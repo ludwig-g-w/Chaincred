c7f3a8d4e9280a5dcacc05141a7f833939f96e45
ludwigsw <ludwig.wiren@rocker.coml>
2023-12-22 16:36:09 +0100

    working maps with callout

--- lib/components/MapView.tsx
+++ lib/components/MapView.tsx
@@ -1,14 +1,14 @@
 import {
+  Badge,
+  BadgeText,
   Box,
-  Button,
+  Center,
+  Pressable,
   Text,
-  Tooltip,
-  TooltipContent,
-  TooltipText,
 } from "@gluestack-ui/themed";
-import React, { useRef, useState, useEffect } from "react";
-import { StyleSheet, Dimensions } from "react-native";
-import MapView, { Circle, Marker } from "react-native-maps";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView, { Callout, Circle, Marker, Region } from "react-native-maps";
 import Supercluster from "supercluster";
 
 const MapComponent = ({ coordinates }: { coordinates?: string[] }) => {
@@ -19,7 +19,6 @@ const MapComponent = ({ coordinates }: { coordinates?: string[] }) => {
 
   // Initialize the supercluster
   const cluster = new Supercluster({
-    log: true,
     radius: 40,
     maxZoom: 16,
   });
@@ -35,7 +34,7 @@ const MapComponent = ({ coordinates }: { coordinates?: string[] }) => {
         properties: { cluster: false, id },
         geometry: {
           type: "Point",
-          coordinates: [Number(long), Number(lat)],
+          coordinates: [Number(lat), Number(long)],
         },
       } as Supercluster.PointFeature<Supercluster.AnyProps>;
     });
@@ -43,61 +42,73 @@ const MapComponent = ({ coordinates }: { coordinates?: string[] }) => {
   }, [coordinates]);
 
   // Function to estimate the zoom level based on latitude delta
-  const getZoomLevel = (latitudeDelta) => {
+  const getZoomLevel = (latitudeDelta: number) => {
     const angle = latitudeDelta;
     return Math.round(Math.log(360 / angle) / Math.LN2);
   };
 
-  // Render the markers and clusters
-  const renderMarkers = () => {
-    return clusters.map((feature, index) => {
-      const coord = {
-        latitude: feature.geometry.coordinates[1],
-        longitude: feature.geometry.coordinates[0],
-      };
-
-      if (feature.properties.cluster) {
-        return (
-          <Marker
-            key={`cluster-${index}`}
-            coordinate={coord}
-            title={`Cluster of ${feature.properties.point_count} items`}
-          />
-        );
-      }
-
-      return (
-        <Marker key={`marker-${feature.properties.id}`} coordinate={coord} />
-      );
-    });
-  };
+  const onRegionChange = useCallback((region: Region) => {
+    const zoom = getZoomLevel(region.latitudeDelta);
+    const bbox = [
+      region.longitude - region.longitudeDelta / 2, // westLng - min lng
+      region.latitude - region.latitudeDelta / 2, // southLat - min lat
+      region.longitude + region.longitudeDelta / 2, // eastLng - max lng
+      region.latitude + region.latitudeDelta / 2, // northLat - max lat
+    ] as [number, number, number, number];
+    const newClusters = cluster.getClusters(bbox, zoom);
+    setClusters(newClusters);
+  }, []);
 
   return (
     <MapView
       ref={mapRef}
       style={styles.map}
-      // Function to update clusters based on current map view
-      onRegionChangeComplete={(region) => {
-        const zoom = getZoomLevel(region.latitudeDelta);
-        const bbox = [
-          region.longitude - region.longitudeDelta / 2, // westLng - min lng
-          region.latitude - region.latitudeDelta / 2, // southLat - min lat
-          region.longitude + region.longitudeDelta / 2, // eastLng - max lng
-          region.latitude + region.latitudeDelta / 2, // northLat - max lat
-        ] as [number, number, number, number];
-
-        try {
-          const newClusters = cluster.getClusters(bbox, zoom);
-          console.log("SUCCESS", zoom, bbox);
-
-          setClusters(newClusters);
-        } catch (error) {
-          console.log("FAIL", zoom, bbox);
-          console.log(error);
-        }
-      }}
+      onRegionChangeComplete={onRegionChange}
     >
-      {renderMarkers()}
+      {clusters.map((feature, index) => {
+        const coord = {
+          latitude: feature.geometry.coordinates[1],
+          longitude: feature.geometry.coordinates[0],
+        };
+
+        if (feature.properties.cluster) {
+          return (
+            <Marker
+              key={`cluster-${index}`}
+              coordinate={coord}
+              title={`Cluster of ${feature.properties.point_count} items`}
+            >
+              <Box
+                aspectRatio={1}
+                zIndex={99}
+                p="$1"
+                borderRadius="$2xl"
+                bgColor="$fuchsia400"
+              >
+                <Center>
+                  <Text color="$white">{feature.properties.point_count}</Text>
+                </Center>
+              </Box>
+            </Marker>
+          );
+        }
+
+        return (
+          <Marker key={`marker-${feature.properties.id}`} coordinate={coord}>
+            <Callout
+              onPress={() => {
+                console.log("asdsad");
+              }}
+              tooltip
+            >
+              <Box bgColor="white" p="$4">
+                <Text>Cluster Info</Text>
+                <Text>Items: {feature.properties.point_count}</Text>
+              </Box>
+            </Callout>
+          </Marker>
+        );
+      })}
     </MapView>
   );
 };
