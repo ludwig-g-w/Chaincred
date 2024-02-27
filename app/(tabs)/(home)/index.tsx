226696a267db314d0cdf21dbc930adca524f38f2
ludwigsw <ludwig.wiren@rocker.coml>
2024-02-27 13:58:06 +0100

    init

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,17 +1,18 @@
-import ListItem from "@components/ProfileCard";
-import { ORGANIZATION_MANAGER_ADDRESS } from "@env";
-import { Box, Text } from "@gluestack-ui/themed";
-import { getProfilesByAddresses } from "@services/supabase";
-import { format, parseISO } from "date-fns";
-import { FlashList } from "@shopify/flash-list";
-import { useAddress, useContract } from "@thirdweb-dev/react-native";
-import { groupAttestationsByAttester } from "@utils/attestations";
-import { ProfileListItem, isAttestItem } from "@utils/types";
-import { router } from "expo-router";
-import React, { useEffect, useMemo, useState } from "react";
-import { useHomeFeedSuspenseQuery } from "../../../generated/graphql";
 import AttestationItem from "@components/AttestationItem";
 import ReviewListItem from "@components/ReviewListItem";
+import { ORGANIZATION_MANAGER_ADDRESS } from "@env";
+import { Box, Button, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { FlashList } from "@shopify/flash-list";
+import {
+  useAddress,
+  useContract,
+  useLogin,
+  useUser,
+} from "@thirdweb-dev/react-native";
+import { ProfileListItem, isAttestItem } from "@utils/types";
+import { format, parseISO } from "date-fns";
+import React, { useMemo, useState } from "react";
+import { useHomeFeedSuspenseQuery } from "../../../generated/graphql";
 
 // Dummy data for restaurants, replace with your actual data source
 const data = {
@@ -50,11 +51,34 @@ const Companies = () => {
     );
   }, [data?.attestations]);
 
+  // TEST
+  const [secret, setSecret] = useState();
+  const { user, isLoggedIn } = useUser();
+  const { login } = useLogin();
+
+  const getSecret = async () => {
+    const res = await fetch("/api/secret");
+    const data = await res.json();
+    setSecret(data.message);
+  };
+
   return (
     <Box px="$2" flex={1} bg="$white">
       <Text color="$textLight600" my="$4" size="lg" bold>
         All Activity
       </Text>
+      <HStack gap="$2">
+        <Button onPress={login}>
+          <Text>Login</Text>
+        </Button>
+        <Button onPress={getSecret}>
+          <Text>Get secret</Text>
+        </Button>
+      </HStack>
+      <VStack gap="$4">
+        <Text>User: {JSON.stringify(user, undefined, 2) || "N/A"}</Text>
+        <Text>Secret: {secret}</Text>
+      </VStack>
       <FlashList
         numColumns={1}
         estimatedItemSize={88}
