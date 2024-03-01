945d1a5e385f4f6871ddeb0018c8a5a3f7e25fd2
ludwigsw <ludwig.wiren@rocker.coml>
2024-03-01 12:13:51 +0100

    env

--- app/api/auth/[...thirdweb]+api.ts
+++ app/api/auth/[...thirdweb]+api.ts
@@ -8,9 +8,7 @@ const users: Record<string, any> = {};
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN || "",
-  wallet: new PrivateKeyWallet(
-    "cac51f3a1eddc7d0bc1a3a10ae24276fea5e27f9e1e4b582ff91ae1cbcbae08a" || ""
-  ),
+  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
   // NOTE: All these callbacks are optional! You can delete this section and
   callbacks: {
     onLogin: async (address, ...rest) => {
@@ -22,15 +20,7 @@ export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
       // We can also provide any session data to store in the user's session.
       return { role: ["admin"] };
     },
-    onUser: async (user) => {
-      // And we can provide any extra user data to be sent to the client
-      // along with the default user object.
-      // let res;
-      // try {
-      //   res = await getProfileByAddress(user.address);
-      // } catch (error) {}
-      // return res;
-    },
+    onUser: async (user) => {},
     onLogout: async (user) => {
       const maybeKEy = AsyncStorage.getItem("auth_token_storage_key");
       console.log({ maybeKEy });
