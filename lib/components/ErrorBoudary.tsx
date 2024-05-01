ded4b268b7ba28b3675f98be772c58311e4c64eb
ludwigsw <ludwig.wiren@rocker.coml>
2024-05-01 19:12:32 +0200

    reinstall everything

--- lib/components/ErrorBoudary.tsx
+++ lib/components/ErrorBoudary.tsx
@@ -1,22 +1,27 @@
 import { Box, Button, Center, Text } from "@gluestack-ui/themed";
 import { ErrorBoundary } from "react-error-boundary";
+import { ScrollView } from "react-native";
 // @ts-ignore
 const MyErrorBoundary = ({ children }) => {
   return (
     <ErrorBoundary
       fallbackRender={({ error, resetErrorBoundary }) => {
         return (
-          <Box bgColor="$red800" flex={1}>
-            <Center>
-              <Text color="white" bold fontSize={"$2xl"}>
-                Something went wrong
-              </Text>
-              <Text mt="$4">{JSON.stringify(error)}</Text>
-              <Button onPress={resetErrorBoundary} mt="auto">
-                <Text color="white">Reset</Text>
-              </Button>
-            </Center>
-          </Box>
+          <ScrollView>
+            <Box bgColor="$red900" flex={1}>
+              <Center>
+                <Text color="white" bold fontSize={"$2xl"}>
+                  Something went wrong
+                </Text>
+                <Text color="white" mt="$4">
+                  {JSON.stringify(error)}
+                </Text>
+                <Button onPress={resetErrorBoundary} mt="auto">
+                  <Text color="white">Reset</Text>
+                </Button>
+              </Center>
+            </Box>
+          </ScrollView>
         );
       }}
       onReset={(details) => {
