6af9285206a7f198b2dff1b00ca488f1910c76c4
ludwigsw <ludwig.wiren@rocker.coml>
2025-03-11 20:45:15 +0100

    removed ai features

--- app/(tabs)/(chat)/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Chat from "./Chat";
-
-// Layout for the Chat tab
-export default function ChatLayout() {
-  return <Chat />;
-}
