3fc704793116cea07ab6294ae4b6ba13c9d34cf5
ludwigsw <ludwig.wiren@rocker.coml>
2025-03-12 08:28:12 +0100

    SafeAreaView for Tabs

--- lib/screens/tabs-layout/LayoutTabs.tsx
+++ lib/screens/tabs-layout/LayoutTabs.tsx
@@ -3,6 +3,7 @@ import { NAV_THEME } from "@lib/constants";
 import { useColorScheme } from "@lib/useColorScheme";
 import { Tabs } from "expo-router";
 import { Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabLayout() {
   const { isDarkColorScheme } = useColorScheme();
@@ -10,89 +11,94 @@ export default function TabLayout() {
   const theme = NAV_THEME[isDarkColorScheme ? "dark" : "light"];
 
   return (
-    <Tabs
-      initialRouteName="(home)"
-      screenOptions={{
-        tabBarActiveTintColor: theme.primary,
-        headerShown: false,
-        header: () => null,
-        tabBarStyle: {
-          backgroundColor: theme.background,
-          borderTopColor: theme.border,
-        },
-      }}
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: theme.background }}
+      edges={["top"]}
     >
-      <Tabs.Screen
-        name="(home)"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <NWIcon
-              className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
-              name={Platform.OS === "ios" ? "house.fill" : "house-chimney"}
-              tintColor={color}
-              type="hierarchical"
-              color={color}
-              size={24}
-            />
-          ),
+      <Tabs
+        initialRouteName="(home)"
+        screenOptions={{
+          tabBarActiveTintColor: theme.primary,
+          headerShown: false,
+          header: () => null,
+          tabBarStyle: {
+            backgroundColor: theme.background,
+            borderTopColor: theme.border,
+          },
         }}
-      />
+      >
+        <Tabs.Screen
+          name="(home)"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <NWIcon
+                className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
+                name={Platform.OS === "ios" ? "house.fill" : "house-chimney"}
+                tintColor={color}
+                type="hierarchical"
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="(discover)"
-        options={{
-          title: "Discover",
-          tabBarIcon: ({ color }) => (
-            <NWIcon
-              className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
-              name={Platform.OS === "ios" ? "binoculars.fill" : "binoculars"}
-              tintColor={color}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="scanAddress"
-        options={{
-          title: "Scan",
-          tabBarIcon: ({ color }) => (
-            <NWIcon
-              className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
-              name={Platform.OS === "ios" ? "camera.fill" : "camera"}
-              tintColor={color}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(settings)"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <NWIcon
-              className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
-              name={Platform.OS === "ios" ? "gearshape.fill" : "gear"}
-              tintColor={color}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(chat)"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <NWIcon name="ellipsis.bubble" color={color} tintColor={color} />
-          ),
-          tabBarLabel: "Chat",
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="(discover)"
+          options={{
+            title: "Discover",
+            tabBarIcon: ({ color }) => (
+              <NWIcon
+                className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
+                name={Platform.OS === "ios" ? "binoculars.fill" : "binoculars"}
+                tintColor={color}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="scanAddress"
+          options={{
+            title: "Scan",
+            tabBarIcon: ({ color }) => (
+              <NWIcon
+                className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
+                name={Platform.OS === "ios" ? "camera.fill" : "camera"}
+                tintColor={color}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(settings)"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <NWIcon
+                className={Platform.OS === "ios" ? "aspect-square, w-8" : ""}
+                name={Platform.OS === "ios" ? "gearshape.fill" : "gear"}
+                tintColor={color}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(chat)"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <NWIcon name="ellipsis.bubble" color={color} tintColor={color} />
+            ),
+            tabBarLabel: "Chat",
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
